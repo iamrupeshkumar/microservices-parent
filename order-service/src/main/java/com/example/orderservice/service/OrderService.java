978defacdc6f957b44f127b2aa81aa9230398267
iamrupeshkumar <rupeshkumar.sde@gmail.com>
2026-02-10 11:43:31 +0530

    In this commit lombok steup done and working fine all over the services

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -3,15 +3,13 @@ package com.example.orderservice.service;
 import com.example.dto.UserDto;
 import com.example.orderservice.client.UserClient;
 import com.example.orderservice.response.OrderResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
     private final UserClient userClient;
-
-    public OrderService(UserClient userClient) {
-        this.userClient = userClient;
-    }
 
     public OrderResponse createOrder(Long userId){
         UserDto userDto = userClient.getUserById(userId);
